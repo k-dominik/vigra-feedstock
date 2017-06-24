c6959fceaec438175a3ae5e44c48027b1252fb46
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-24 16:40:57 -0400

    Add a basic `bld.bat` using CMake
    Provides a simple `bld.bat`, which builds the library with CMake. Also
    runs the Python tests. Basically a simplified version of the build used
    for macOS and Linux.

---
+++ recipe/bld.bat
@@ -0,0 +1,23 @@
+mkdir build
+if errorlevel 1 exit 1
+
+cd build
+if errorlevel 1 exit 1
+
+cmake .. ^
+        -G "NMake Makefiles" ^
+        -DCMAKE_BUILD_TYPE="Release" ^
+        -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+        -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+        -DBUILD_SHARED_LIBS=1 ^
+        "%SRC_DIR%"
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake check_python
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
